7f5213edef552413bbade8e9fa223167240ea069
ΛTᏥΛᏒᐯΛ <115201572+atharv0905@users.noreply.github.com>
2023-11-18 20:12:08 +0530

    FPS Camera
    Setting up basic FPS Camera

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,10 +6,15 @@ public class Player : MonoBehaviour
 {
     [SerializeField] public float speed;
     [SerializeField] public CharacterController controller;
+    [SerializeField] public float mouseSensitivity;
+    private float cameraVerticalRotation = 0;
+    [SerializeField] public Transform camera;
 
     private string HORIZONTAL_KEY = "Horizontal";
+    private string HORIZONTAL_RAW_KEY = "Mouse X";
     private string VERTICAL_KEY = "Vertical";
-    
+    private string VERTICAL_RAW_KEY = "Mouse Y";
+
 
     void Start()
     {
@@ -20,6 +25,12 @@ public class Player : MonoBehaviour
     void Update()
     {
         PlayerMovement();
+        float mouseX = Input.GetAxisRaw(HORIZONTAL_RAW_KEY) * Time.deltaTime* mouseSensitivity;
+        float mouseY = Input.GetAxisRaw(VERTICAL_RAW_KEY) * Time.deltaTime * mouseSensitivity;
+
+        cameraVerticalRotation = cameraVerticalRotation - mouseY;
+        transform.Rotate(Vector3.up * mouseX);
+        camera.localRotation = Quaternion.Euler(cameraVerticalRotation, 0f, 0f);
     }
 
     void PlayerMovement()
