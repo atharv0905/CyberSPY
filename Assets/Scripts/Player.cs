4d4ca7856abb5a39f7de7dee4b5f4a85eb5aa00e
ΛTᏥΛᏒᐯΛ <115201572+atharv0905@users.noreply.github.com>
2023-11-19 19:01:34 +0530

    Added jump
    Added character jump functionality

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,26 +9,38 @@ public class Player : MonoBehaviour
     public float speed;
     public float autoFireRate;
 
+    // gravity variables
     public Vector3 velocity;
     public float gravityModifier;
 
+    // camera movement variables
     public CharacterController controller;
     public Transform myCameraHead;
 
     public float mouseSensitivity;
     private float cameraVerticalRotation = 0;
 
-    private string HORIZONTAL_KEY = "Horizontal";
-    private string HORIZONTAL_RAW_KEY = "Mouse X";
-    private string VERTICAL_KEY = "Vertical";
-    private string VERTICAL_RAW_KEY = "Mouse Y";
+    // input key constants
+    private const string HORIZONTAL_KEY = "Horizontal";
+    private const string HORIZONTAL_RAW_KEY = "Mouse X";
+    private const string VERTICAL_KEY = "Vertical";
+    private const string VERTICAL_RAW_KEY = "Mouse Y";
+    private const string JUMP_KEY = "Jump";
 
+    // firing variables
     public GameObject bullet;
     public Transform firePoint;
 
     public GameObject muzzleFlash;
     public GameObject bulletHole;
     public GameObject waterLeak;
+
+    // jumping variables
+    public float jumpHeight;
+    private bool readyToJump;
+    public Transform ground;
+    public LayerMask groundLayer;
+    public float groundDistance;
 
     void Start()
     {
@@ -41,7 +53,18 @@ public class Player : MonoBehaviour
         PlayerMovement();
         CameraMovement();
         Shoot();
-        Invoke(nameof(AutoFire), autoFireRate);
+        Jump();
+        //Invoke(nameof(AutoFire), autoFireRate);
+    }
+
+    private void Jump()
+    {
+        readyToJump = Physics.OverlapSphere(ground.position, groundDistance, groundLayer).Length > 0;
+        if (Input.GetButtonDown(JUMP_KEY) && readyToJump)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * Physics.gravity.y) * Time.deltaTime;
+        }
+        controller.Move(velocity);
     }
 
     private void AutoFire()
